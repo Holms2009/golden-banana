932c4b9535f26bac336b07a216cd5f9d3fa2de15
Holms2009 <holms200990@gmail.com>
2022-02-12 22:38:29 +0300

    Links fix

--- src/blocks/NavMenu/NavMenu.tsx
+++ src/blocks/NavMenu/NavMenu.tsx
@@ -12,11 +12,12 @@ type Props = {
 }
 
 const NavMenu = ({ menuItems, direction }: Props) => {
+  const isDev = process.env.NODE_ENV === 'development' ? true : false;
 
   return (
     <nav className={b({ horizontal: direction === 'horizontal', vertical: direction === 'vertical' })}>
       {menuItems.map((item, index) => (
-        <Link className={b('item')} to={item.link} key={index}>
+        <Link className={b('item')} to={isDev ? item.link : '/golden-banana' + item.link} key={index}>
           <img className={b('icon')} src={item.image} alt="Иуонка пункта меню" />
           <p className={b('text')}>{item.text}</p>
         </Link>
